448789898d992a8a38a4a7e5da675a6655845240
Shih-Wei Guo <tony2guo@gmail.com>
2021-05-11 00:42:47 +0800

    disable box on start

---
+++ Assets/Scripts/DisableBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableBox : MonoBehaviour
+{
+    public GameObject box;
+    // public GameObject answer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        disable_box();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void disable_box()
+    {
+        box.SetActive(false);
+    }
+}
