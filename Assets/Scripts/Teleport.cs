a34240f0c6b76f135645819ffdbd9b0f5edafb83
Shih-Wei Guo <tony2guo@gmail.com>
2021-05-10 23:33:39 +0800

    object teleport

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform TeleportTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag != "Player")
+        {
+            Debug.Log("Teleport");
+            collision.transform.position = TeleportTransform.position + Vector3.up * 1.0f;
+        }
+    }
+}
