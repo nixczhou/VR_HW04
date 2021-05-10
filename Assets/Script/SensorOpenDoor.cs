4eb83af83b56ae6fa7e50496d2a6c9bb48ee4679
Shih-Wei Guo <tony2guo@gmail.com>
2021-05-10 23:11:42 +0800

    open door

--- Assets/Script/SensorOpenDoor.cs
+++ Assets/Script/SensorOpenDoor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SensorOpenDoor : MonoBehaviour
 {
+    public Transform door1;
+    public Transform door2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,35 @@ public class SensorOpenDoor : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.up, out hit))
+        {
+            // Debug.Log("Hit");
+            // if (hit.transform.gameObject.tag == "Player")
+            // {
+                // open door
+            if(door1.position.y < 9.0f)
+            {
+                Vector3 dy = Vector3.up * 4.0f * Time.deltaTime;
+                door1.position = door1.position + dy;
+                door2.position = door2.position + dy;
+            }
+            // }
+        }
+        else
+        {
+            // close door
+            if(door1.position.y > 0.0f)
+            {
+                Vector3 dy = Vector3.up * -4.0f * Time.deltaTime;
+                door1.position = door1.position + dy;
+                door2.position = door2.position + dy;
+            }
+        }
     }
 }
