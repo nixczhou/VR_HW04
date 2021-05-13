782938db0d698c6f5fd89e658eb7c20f676564ca
Shing Ming <shingming1998@icloud.com>
2021-05-14 01:02:41 +0800

    Init location and player move

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManger : MonoBehaviour
+{
+    public Transform teamOneTransform;
+    public Transform teamTwoTransform;
+
+    private GameObject localPlayer = null;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        InstantiatePlayer();
+    }
+
+    [PunRPC]
+    void InstantiatePlayer()
+    {
+        ExitGames.Client.Photon.Hashtable playerCustomProperties = PhotonNetwork.LocalPlayer.CustomProperties;
+
+        if (playerCustomProperties["Team"].ToString().Equals("team1"))
+        {
+            localPlayer = PhotonNetwork.Instantiate("Player_Team1", teamOneTransform.position, Quaternion.identity, 0);
+        }
+        else if (PhotonNetwork.LocalPlayer.CustomProperties["Team"].ToString().Equals("team2"))
+        {
+            localPlayer = PhotonNetwork.Instantiate("Player_Team2", teamTwoTransform.position, Quaternion.identity, 0);
+        }
+
+        Transform tempTransform = localPlayer.transform;
+        mainCamera.transform.parent = tempTransform;
+        mainCamera.transform.localPosition = new Vector3(0, 0, 0);
+        mainCamera.transform.localRotation = Quaternion.identity;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float moveSpeed = 6.0f;
+	public float rotateSpeed = 10.0f;
+	//public float jumpVelocity = 5.0f;
+
+	private float minMouseRotateX = -45.0f;
+    private float maxMouseRotateX = 45.0f;
+    private float mouseRotateX;
+	
+	Camera myCamera;
+    //Rigidbody rigid;
+    //bool isGrounded;
+    //CapsuleCollider capsuleCollider;
+
+    void Start()
+    {
+		myCamera = Camera.main;
+		mouseRotateX = myCamera.transform.localEulerAngles.x;
+		//rigid = GetComponent<Rigidbody> ();
+		//capsuleCollider = GetComponent<CapsuleCollider> ();
+	}
+    
+	void Update()
+	{
+        if (GetComponent<Photon.Pun.PhotonView>().IsMine == true)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            Move(h, v);
+            float rv = Input.GetAxisRaw("Mouse X");
+            float rh = Input.GetAxisRaw("Mouse Y");
+            Rotate(rh, rv);
+        }
+	}
+    
+	void Move(float h,float v)
+    {
+		transform.Translate ((Vector3.forward * v + Vector3.right * h) * moveSpeed * Time.deltaTime);
+	}
+
+	void Rotate(float rh,float rv)
+    {
+		transform.Rotate (0, rv * rotateSpeed, 0);
+		mouseRotateX -= rh * rotateSpeed;
+		mouseRotateX = Mathf.Clamp (mouseRotateX, minMouseRotateX, maxMouseRotateX);
+		myCamera.transform.localEulerAngles = new Vector3 (mouseRotateX, 0.0f, 0.0f);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
