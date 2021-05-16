5e332089d16cddfdf50fe1d83b293bfa158c7c37
nixczhou <nicozhou@icloud.com>
2021-05-16 21:39:37 +0800

    can see player name

---
+++ Assets/Scripts/ShowPlayerName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+public class ShowPlayerName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string nickname = PhotonNetwork.LocalPlayer.NickName;
+        Debug.Log(nickname);
+        TextMeshPro mText = gameObject.GetComponent<TextMeshPro>();
+        mText.text = nickname;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
